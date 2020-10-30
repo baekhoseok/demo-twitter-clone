e3a0b9783c02669264a1cace91d4d6c01de7d4c7
baekhoseok <baekhoseok@MacBook-Pro.local>
2020-10-30 15:12:16 +0900

    delete short id

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import userSaga from './user';
 import postSaga from './post';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://54.180.25.148:9365/';
 
 export default function* rootSaga() {
   yield all([fork(userSaga), fork(postSaga)]);
